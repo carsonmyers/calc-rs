8620147165c1486bc9963537301f4f97601b42ec
Carson Myers <carson@myers.se>
2023-04-06 15:04:13 +0200

    reconnected read_to_ast

--- src/translate/mod.rs
+++ src/translate/mod.rs
@@ -12,13 +12,15 @@ pub use ir::Instruction;
 use ir::ToIr;
 pub use parser::Parser;
 
-pub fn read_to_ast(input: &str) -> Result<Option<Expr>> {
-    //let mut parser = Parser::new(input);
-    //parser.parse()
-    unimplemented!()
+use crate::input::Input;
+
+pub fn read_to_ast(mut input: Input) -> Result<Option<Expr>> {
+    let mut tokenizer = tokenizer::Tokenizer::new(&mut input);
+    let mut parser = Parser::new(tokenizer.into());
+    parser.parse()
 }
 
-pub fn read_to_ir(input: &str) -> Result<Option<Vec<Instruction>>> {
+pub fn read_to_ir(input: Input) -> Result<Option<Vec<Instruction>>> {
     if let Some(ast) = read_to_ast(input)? {
         Ok(Some(ast.to_ir()))
     } else {
